5d38dbbac024cf35d597bd04a42a21f64746082f
Nurfihartono <nurfihartonosaflin@gmail.com>
2025-07-08 18:03:39 +0800

    navbar

---
+++ php/client/beranda.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Beranda | Sistem Manajemen Lab</title>
+    <link rel="stylesheet" href="../css/beranda.css">
+</head>
+<body>
+    
+</body>
+</html>
---
+++ php/client/navbar.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="../../css/navbar.css">
+</head>
+<body>
+    <header>
+        <h1>sistem Manajemen lab</h1>
+        <nav>
+            <ul>
+                <li><a href="beranda.php">beranda</a></li>
+                <li><a href="inventaris.php">inventaris</a></li>
+                <li><a href="peminjaman.php">peminjaman</a></li>
+                <li><a href="tentang.php">tentang kami</a></li>
+                <li><a href="kontak.php">kontak</a></li>
+            </ul>
+        </nav>
+        <div class="logout-btn">
+            <a href="logout.php">logout</a>
+        </div>
+</header>
+</body>
+</html>
